9ff32fb5e37dd89b2dbfc08218af534a1103ca38
German Gorodnev <germangorodnev@gmail.com>
2023-01-20 18:50:54 +0100

    testing esbuild

--- src/preset.js
+++ src/preset.js
@@ -1,4 +1,48 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable global-require */
 const browserslist = require('browserslist')
+
+/**
+ * Returns the latest
+ * vendor version
+ * @param {String} vendor
+ * @return {Number} version
+ */
+function latest(vendor) {
+  const { versions } = browserslist.data[vendor]
+  return versions[versions.length - 1]
+}
+
+/**
+ * Returns the appropriate
+ * vendor version to target
+ * @param {String} vendor
+ * @param {String} version
+ * @return {Number}
+ */
+function getTargetVendorVersion(vendor, version) {
+  // Return the specified version if it is numeric
+  if (!Number.isNaN(version)) {
+    return parseInt(version, 10)
+  }
+
+  // Default to the latest version of each browser
+  let query = 'last 1 version';
+
+  // For "auto" use project's config restricted to target vendor.
+  // If target vendor is not in the project's config, use defaults
+  if (typeof version === 'string' && version.toLowerCase() === 'auto') {
+    query = `browserslist config and ${vendor} > 0 or defaults`
+  }
+
+  // The last value returned by `browserslist()` is the "oldest" that matches the query
+  const browserString = browserslist(query).filter(browser => browser.indexOf(vendor) !== -1).pop()
+
+  // Convert the browser string (ex "chrome 67") to just the version number
+  return parseInt(browserString.replace(`${vendor  } `, ''), 10)
+}
+
+exports.getTargetByVendor = ({ vendor, vendorVersion }) => `${vendor}${getTargetVendorVersion(vendor, vendorVersion)}`
 
 module.exports = ({ vendor, vendorVersion }) => {
   const env = process.env.BABEL_ENV || process.env.NODE_ENV
@@ -71,44 +115,4 @@ module.exports = ({ vendor, vendorVersion }) => {
       }]
     ].filter(Boolean)
   }
-}
-
-/**
- * Returns the latest
- * vendor version
- * @param {String} vendor
- * @return {Number} version
- */
-function latest(vendor) {
-  const { versions } = browserslist.data[vendor]
-  return versions[versions.length - 1]
-}
-
-/**
- * Returns the appropriate
- * vendor version to target
- * @param {String} vendor
- * @param {String} version
- * @return {Number}
- */
-function getTargetVendorVersion(vendor, version) {
-  // Return the specified version if it is numeric
-  if (!isNaN(version)) {
-    return parseInt(version, 10)
-  }
-
-  // Default to the latest version of each browser
-  let query = 'last 1 version';
-
-  // For "auto" use project's config restricted to target vendor.
-  // If target vendor is not in the project's config, use defaults
-  if (typeof version === 'string' && version.toLowerCase() === 'auto') {
-    query = `browserslist config and ${vendor} > 0 or defaults`
-  }
-
-  // The last value returned by `browserslist()` is the "oldest" that matches the query
-  const browserString = browserslist(query).filter(browser => browser.indexOf(vendor) !== -1).pop()
-
-  // Convert the browser string (ex "chrome 67") to just the version number
-  return parseInt(browserString.replace(`${vendor  } `, ''), 10)
 }
--- src/webpack-config.js
+++ src/webpack-config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
 const { resolve } = require('path')
 const webpack = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
@@ -5,9 +7,9 @@ const GlobEntriesPlugin = require('webpack-watched-glob-entries-plugin')
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const WebextensionPlugin = require('webpack-webextension-plugin')
-const getExtensionInfo = require('./utils/get-extension-info')
-const createPreset = require('./preset')
 const WebpackBar = require('webpackbar')
+const getExtensionInfo = require('./common/utils/getExtensionInfo')
+const createPreset = require('./preset')
 
 module.exports = function webpackConfig({
   src = 'app',
@@ -32,9 +34,9 @@ module.exports = function webpackConfig({
   // Get some defaults
   const { version, name, description } = getExtensionInfo(src)
 
-  /******************************/
+  /** *************************** */
   /*      WEBPACK               */
-  /******************************/
+  /** *************************** */
   const config = {
     mode,
     context: resolve(src)
@@ -48,9 +50,9 @@ module.exports = function webpackConfig({
   // Source-Maps
   config.devtool = devtool
 
-  /******************************/
+  /** *************************** */
   /*       WEBPACK.ENTRY        */
-  /******************************/
+  /** *************************** */
   const entries = []
 
   // Add main entry glob
@@ -64,35 +66,44 @@ module.exports = function webpackConfig({
     entries
   )
 
-  /******************************/
+  /** *************************** */
   /*       WEBPACK.OUTPUT       */
-  /******************************/
+  /** *************************** */
   config.output = {
     path: target,
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   }
-  /******************************/
+  /** *************************** */
   /*    WEBPACK.OPTIMIZATION    */
-  /******************************/
-  config.optimization = { minimize: minimize }
+  /** *************************** */
+  config.optimization = { minimize }
 
-  /******************************/
+  /** *************************** */
   /*       WEBPACK.LOADERS      */
-  /******************************/
+  /** *************************** */
   config.module = {
     rules: []
   }
 
-  const babelLoader = {
-    loader: require.resolve('babel-loader'),
+  // const babelLoader = {
+  //   loader: require.resolve('babel-loader'),
+  //   options: {
+  //     sourceMaps: true,
+  //     cacheDirectory: true,
+  //     ...createPreset({
+  //       vendor,
+  //       vendorVersion
+  //     })
+  //   }
+  // };
+
+  const compileTarget = createPreset.getTargetByVendor({ vendor, vendorVersion });
+  const esbuildLoader = {
+    loader: require.resolve('esbuild-loader'),
     options: {
-      sourceMaps: true,
-      cacheDirectory: true,
-      ...createPreset({
-        vendor,
-        vendorVersion
-      })
+      loader: 'tsx',
+      target: compileTarget,
     }
   };
 
@@ -104,7 +115,7 @@ module.exports = function webpackConfig({
         /node_modules/,
         resolve(process.cwd(), 'app/scripts/fontawesome.js'),
       ],
-      use: babelLoader,
+      use: esbuildLoader,
     },
     {
       test: /\.js$/,
@@ -123,9 +134,9 @@ module.exports = function webpackConfig({
     // }
   )
 
-  /******************************/
+  /** *************************** */
   /*     WEBPACK.PLUGINS        */
-  /******************************/
+  /** *************************** */
   config.plugins = []
 
   // Clear output directory
